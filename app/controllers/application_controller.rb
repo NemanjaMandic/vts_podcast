ac97a05187d59e16b8c02fc00991c257b0fa3f2d
NemanjaMandic <nemanjans84@gmail.com>
2015-12-07 19:59:49 +0100

    Ability to add images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation,
-  		 :current_password, :title, :description, :itunes, :stitcher, :podbay)} 
+  		 :current_password, :title, :description, :itunes, :stitcher, :podbay, :thumbnail)} 
 
   		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation,
-  		  :title)} 
+  		  :title, :thumbnail)} 
   end
 
 end
